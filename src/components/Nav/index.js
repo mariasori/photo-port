9f29f1bd4bfbaa1b775473157c5bcd8de6c5a19b
Maria Sori <mariasori.school@gmail.com>
2022-01-19 23:18:35 -0500

    nav tests

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const categories = [
   {
-    name: "commercial",
+    name: "Commercial",
     description:
       "Photos of grocery stores, food trucks, and other commercial projects",
   },
-  { name: "portraits", description: "Portraits of people in my life" },
-  { name: "food", description: "Delicious delicacies" },
+  { name: "Portraits", description: "Portraits of people in my life" },
+  { name: "Food", description: "Delicious delicacies" },
   {
-    name: "landscape",
+    name: "Landscape",
     description: "Fields, farmhouses, waterfalls, and the beauty of nature",
   },
 ];
@@ -23,14 +23,14 @@ function Nav() {
   return (
     <header>
     <h2>
-        <a href="/">
-        <span role="img" aria-label="camera"> 📸</span> Oh Snap!
+        <a data-testid="link" href="/">
+            <span role="img" aria-label="camera"> 📸</span> Oh Snap!        
         </a>
     </h2>
     <nav>
         <ul className="flex-row">
         <li className="mx-2">
-            <a href="#about">
+            <a data-testid="about" href="#about">
             About me
             </a>
         </li>
